8877a2a9ed8e9cd211d057a14366d11a4b065696
amine larhrissi <codemeforlife@gmail.com>
2018-11-08 20:10:59 +0000

    doses bitch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+  post "/cocktails/:id/cocktail", to: "cocktails#create"
+  delete "/cocktails/:id", to: "cocktails#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'faker'
+
+
+json_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_list = open(json_url).read
+ingredients = JSON.parse(ingredient_list)
+ingredients["drinks"].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
+
+
+50.times { Cocktail.create(name:Faker::Beer.name) }
+
